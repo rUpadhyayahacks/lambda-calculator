481f311df805b282aff96ec0f6423137b835c7ac
R <r@Rs-MacBook-Air.local>
2019-12-11 11:39:49 -0800

    style

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -4,7 +4,7 @@ const NumberButton = (props) => {
   const {buttonNum, setNumDisplay, numDisplay} = props
   return (
     <div className = 'button-numbers'>
-    <button onClick = {()=> setNumDisplay(numDisplay + buttonNum)}>{buttonNum}</button>
+    <p onClick = {()=> setNumDisplay(numDisplay + buttonNum)}>{buttonNum}</p>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
       </div>
   );
