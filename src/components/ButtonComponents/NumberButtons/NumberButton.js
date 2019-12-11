9c4cf98af7625247b263618dd93a15f772eb097b
R <r@Rs-MacBook-Air.local>
2019-12-11 09:43:40 -0800

    first

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,9 +1,13 @@
 import React from "react";
 
-const NumberButton = () => {
+const NumberButton = (props) => {
+  const {buttonNum, setNumDisplay, numDisplay} = props
   return (
-    <>
+    <div className = 'button-numbers'>
+    <button onClick = {()=> setNumDisplay(numDisplay + buttonNum)}>{buttonNum}</button>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
+      </div>
   );
 };
+
+export default NumberButton
--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import OperatorButton  from "./OperatorButton";
+import { operators } from '../../../data';
 
 //import any components needed
 
 //Import your array data to from the provided data file
 
-const Operators = () => {
+const Operators = (props) => {
+  const [operatorsData, setOperatorData] = useState(operators)
   // STEP 2 - add the imported data to state
   return (
     <div>
+      {operatorsData.map ((buttonNum, index) => (
+        <OperatorButton
+          key={index}
+          buttonNum={buttonNum}
+          setNumDisplay={props.setNumDisplay}
+          />
+
+    ))}
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
     </div>
   );
 };
+
+export default Operators
